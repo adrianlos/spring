120f8edbbf7497ebc3d67639f689a002222bdb38
MikiKru <michal_kruczkowski@o2.pl>
2019-06-02 12:00:02 +0200

    v3

--- src/main/java/com/example/rest/controller/MainController.java
+++ src/main/java/com/example/rest/controller/MainController.java
@@ -3,6 +3,7 @@ package com.example.rest.controller;
 import com.example.rest.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -12,6 +13,7 @@ import java.util.*;
 public class MainController {
 
     private List<String> logins = new ArrayList<>(Arrays.asList("mk","kk","ee","rr"));
+    private User user;
 
     @GetMapping("/")
     public String hello(){
@@ -21,9 +23,18 @@ public class MainController {
     public List<String> getLogins(){
         return logins;
     }
-    @PostMapping("/addUser")
-    public User addUser(String login, String password){
-        return new User(login ,password, LocalDateTime.now(),true);
+//    @PostMapping("/addUser")
+//    public User addUser(String login, String password){
+//        user = new User(login ,password, LocalDateTime.now(),false);
+//        return user;
+//    }
+    @PutMapping("/confirmRegistration")
+    public void confirmRegistration(){
+        user.setActive(true);
+    }
+    @GetMapping("/getUser")
+    public User getUser(){
+        return user;
     }
 
 
--- src/main/java/com/example/rest/model/User.java
+++ src/main/java/com/example/rest/model/User.java
@@ -3,14 +3,33 @@ package com.example.rest.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
+@Entity
 public class User {
-    String login;
-    String password;
-    LocalDateTime registration_date;
-    boolean active;
+    @Id                                             // PRIMARY KEY
+    @GeneratedValue(strategy = GenerationType.AUTO) // AUTO_INCREMENT
+    private Long id;
+    private String login;
+    private String password;
+    private LocalDateTime registration_date = LocalDateTime.now();
+    private boolean active = false;
+
+    // RELACJA N:M
+    @ManyToMany(
+            cascade = CascadeType.ALL,                          // pełna rekursywność
+            fetch = FetchType.EAGER                             // zachłanne pobieranie rekordów
+    )
+    @JoinTable(
+            name = "user_role",                                 // nazwa tabelki
+            joinColumns = @JoinColumn(name = "user_id"),        // klucz uzytkownika
+            inverseJoinColumns = @JoinColumn(name = "role_id")  // klucz roli
+    )
+    private Set<Role> roles = new HashSet<>();
 
 }
